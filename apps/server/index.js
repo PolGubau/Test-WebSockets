b7f39baafe565251fd029741c6fb7083b8e09471
Pol Gubau Amores <gubaupol@gmail.com>
2023-10-13 10:12:29 +0200

    first commit

---
+++ apps/server/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import logger from "morgan";
+
+import { Server } from "socket.io";
+import { createServer } from "node:http";
+
+const port = process.env.PORT ?? 3000;
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  connectionStateRecovery: {
+    maxDisconnectionDuration: 1000,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+    io.emit("chat message", msg);
+  });
+});
+
+app.use(logger("dev"));
+
+app.get("/", (req, res) => {
+  res.sendFile(process.cwd() + "/apps/client/index.html");
+});
+
+server.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
